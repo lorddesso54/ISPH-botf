e12cc477810d061948705507d08be46f5d4e4edd
lorddesso54 <lorddesso1520@gmail.com>
2026-05-09 09:51:48 +0800

    Refactor index.js for cleaner code structure

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { joinVoiceChannel } = require('@discordjs/voice');
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
 });
-.
+
 const GUILD_ID = "1125804223049777265";
 const CHANNEL_ID = "1125804223926374453";
 
@@ -12,9 +12,6 @@ client.once('ready', async () => {
   console.log(`Logged in as ${client.user.tag}`);
 
   const guild = await client.guilds.fetch(GUILD_ID);
-  const channel = guild.channels.cache.get(CHANNEL_ID);
-
-  if (!channel) return console.log("Channel not found");
 
   joinVoiceChannel({
     channelId: CHANNEL_ID,
@@ -25,5 +22,4 @@ client.once('ready', async () => {
   console.log("Bot joined VC and will stay there 🧷");
 });
 
-client.login("process.env.TOKEN");            
-
+client.login(process.env.TOKEN);
